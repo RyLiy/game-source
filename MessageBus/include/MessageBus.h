421c61812448775a8fa9a30ea6777cea82894fb3
ryanliy <liyanage.ryan@Gmail.com>
2024-03-30 20:52:13 -0400

    Refactored project to use cmake

---
+++ MessageBus/include/MessageBus.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <optional>
+#include <iostream>
+#include <algorithm>
+
+// Forward declarations
+class MessageBusNode;
+class Message;
+
+class MessageBus {
+public:
+    void addNodes(MessageBusNode* node);
+    void addNodes(std::vector<MessageBusNode*> nodeList);
+    void syncBusNode(std::vector<MessageBusNode*> nodeList);
+    void sendMessage(Message* msg);
+    void notify();
+
+private:
+    std::vector<MessageBusNode*> nodes;
+    std::vector<Message*> messageQueue;
+    std::map<std::vector<MessageBusNode>, std::string> engineMap;
+};
+
+class Message {
+public:
+    Message(std::string event, MessageBusNode* sender, MessageBusNode* component);
+    Message(std::string event, MessageBusNode* sender, std::vector<MessageBusNode*> component);
+    Message(std::string event, MessageBusNode* sender);
+    std::string getMessage();
+    MessageBusNode* getSender();
+    std::vector<MessageBusNode*> getRecipients();
+
+private:
+    std::string event;
+    MessageBusNode* sender;
+    std::vector<MessageBusNode*> componentList;
+};
+
+class MessageBusNode {
+public:
+    virtual void receiveMessage(Message* event);
+    void sendMessage(std::string msg);
+    void sendMessage(std::string msg, MessageBusNode* receiver);
+    void sendMessage(std::string msg, std::vector<MessageBusNode*> receivers);
+    void setBus(MessageBus* bus);
+
+private:
+    MessageBus* bus;
+};
